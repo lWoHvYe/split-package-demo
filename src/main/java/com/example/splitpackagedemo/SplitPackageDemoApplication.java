b2d8d04a40fe976174552fc5da0966eea10d8736
王红岩 lWoHvYe <lwohvye@outlook.com>
2022-04-14 21:49:16 +0800

    Initial commit

---
+++ src/main/java/com/example/splitpackagedemo/SplitPackageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.splitpackagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitPackageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitPackageDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/splitpackagedemo/SplitPackageDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splitpackagedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitPackageDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
