ed1816eb824b3793070ba531613f3fda0d62c995
王红岩 lWoHvYe <lwohvye@outlook.com>
2022-04-14 21:50:49 +0800

    demo

--- src/main/java/com/example/splitpackagedemo/SplitPackageDemoApplication.java
+++ src/main/java/com/example/splitpackagedemo/SplitPackageDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SplitPackageDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SplitPackageDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SplitPackageDemoApplication.class, args);
+    }
 
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module split.pkg.demo {
+    requires io.swagger.v3.oas.annotations;
+    requires io.swagger.v3.oas.models;
+}
+
